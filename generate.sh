4ed2fb626f3c8528bc4b4fcb7297aae4feee1402
Karl Benedict <kbene@unm.edu>
2016-12-09 08:34:57 -0700

    regenerated from most recent version the poster content

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 pandoc -s -S \
 --normalize \
 --filter pandoc-citeproc \
---csl ./elsevier-harvard.csl \
+--csl ./science.csl \
 --template=poster.tex \
 -f markdown \
 -o 2016-12_AGUPoster.pdf \
@@ -10,7 +10,7 @@ pandoc -s -S \
 pandoc -s -S \
 --normalize \
 --filter pandoc-citeproc \
---csl ./elsevier-harvard.csl \
+--csl ./science.csl \
 --template=poster.tex \
 -f markdown \
 -o 2016-12_AGUPoster.tex \
